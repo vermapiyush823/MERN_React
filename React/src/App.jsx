84bfd46707bfa573dae9a1794a4d3ee46efafcc9
vermapiyush823 <vermapiyush823@gmail.com>
2023-11-13 13:53:24 +0530

    commit

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 const user = {
   name:'Piyush',
   Age:19,
+  imgSrc:'https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1699747200&semt=sph',
+  size:200
 };
 
 const App = ()=> {
@@ -10,6 +12,7 @@ const App = ()=> {
     <>
     <h1>Hey, this is <span>{user.name}</span></h1>
     <h2>and, I am <span>19</span></h2>
+    <img src={user.imgSrc} alt="" style={{width:user.size+"px",height:user.size+"px"}}/>
     </>
   )
 }
