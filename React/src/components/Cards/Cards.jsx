449c6e7728e9b2410f42b38059e9af4a4307cec5
vermapiyush823 <vermapiyush823@gmail.com>
2023-11-15 01:46:11 +0530

    commit

---
+++ React/src/components/Cards/Cards.jsx
@@ -0,0 +1,11 @@
+import './Cards.css';
+const Cards = ()=>{
+    return (
+        <div className="card">
+            <p><span className='field'>First Name : </span><span>Piyush</span></p>
+            <p><span className='field'>Last Name : </span><span>Verma</span></p>
+        </div>
+    )
+}
+
+export default Cards
