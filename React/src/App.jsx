53a63b04a875097bb5bd62d636b0b259daded0d0
vermapiyush823 <vermapiyush823@gmail.com>
2023-11-15 12:48:28 +0530

    commit

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -2,45 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
-import Cards from "./components/Cards/Cards";
+import  Home from "./pages/Home/Home"
 
-
-const names = [
-  {
-
-    fName:'Piyush',
-    lName:'Verma'
-  },
-  {
-    
-    fName:'Kashish',
-    lName:'Verma'
-  },
-  {
-    
-    fName:'Sahil',
-    lName:'Verma'
-  },
-  {
-    fName:'Sagar',
-    lName:'Verma'
-  }
-]
 const App = () => {
   return (
     <Routes>
       <Route
         path="/"
-        Component={() => {
-          return (
-            <div className="card-container">
-             { names.map((val,i)=>{
-              return(
-                <Cards key={i} fName={val.fName} lName={val.lName}/>);
-              })}
-            </div>
-          );
-        }}
+        Component={Home}
       />
       <Route path="/about" Component={About} />
       <Route path="/contact" Component={Contact} />
---
+++ React/src/pages/Home/Home.jsx
@@ -0,0 +1,35 @@
+import Cards from "../../components/Cards/Cards";
+import './Home.css';
+const names = [
+    {
+  
+      fName:'Piyush',
+      lName:'Verma'
+    },
+    {
+      
+      fName:'Kashish',
+      lName:'Verma'
+    },
+    {
+      
+      fName:'Sahil',
+      lName:'Verma'
+    },
+    {
+      fName:'Sagar',
+      lName:'Verma'
+    }
+  ]
+const Home = () => {
+    return (
+      <div className="card-container">
+       { names.map((val,i)=>{
+        return(
+          <Cards key={i} fName={val.fName} lName={val.lName}/>);
+        })}
+      </div>
+    );
+  }
+
+export default Home;
