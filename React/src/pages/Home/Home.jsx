a24e2bbf13141fc25edec08a5437bfcf1e5d0433
vermapiyush823 <vermapiyush823@gmail.com>
2023-11-15 12:50:41 +0530

    commit

--- React/src/pages/Home/Home.jsx
+++ React/src/pages/Home/Home.jsx
@@ -1,35 +1,33 @@
 import Cards from "../../components/Cards/Cards";
-import './Home.css';
+import "./Home.css";
 const names = [
-    {
-  
-      fName:'Piyush',
-      lName:'Verma'
-    },
-    {
-      
-      fName:'Kashish',
-      lName:'Verma'
-    },
-    {
-      
-      fName:'Sahil',
-      lName:'Verma'
-    },
-    {
-      fName:'Sagar',
-      lName:'Verma'
-    }
-  ]
+  {
+    fName: "Piyush",
+    lName: "Verma",
+  },
+  {
+    fName: "Kashish",
+    lName: "Verma",
+  },
+  {
+    fName: "Sahil",
+    lName: "Verma",
+  },
+  {
+    fName: "Sagar",
+    lName: "Verma",
+  },
+];
 const Home = () => {
-    return (
+  return (
+    <div className="home">
       <div className="card-container">
-       { names.map((val,i)=>{
-        return(
-          <Cards key={i} fName={val.fName} lName={val.lName}/>);
+        {names.map((val, i) => {
+          return <Cards key={i} fName={val.fName} lName={val.lName} />;
         })}
       </div>
-    );
-  }
+    </div>
+  );
+};
 
 export default Home;
