5647947dc77ff2db02d133289d18e4fed7016eca
vermapiyush823 <vermapiyush823@gmail.com>
2023-11-23 21:19:39 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,11 @@
-console.log("Hi")
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res)=>{
+    res.send("It works!")
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
