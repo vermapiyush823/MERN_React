0744a41ff9511fe3ffe219f071510074f6df3924
vermapiyush823 <vermapiyush823@gmail.com>
2023-11-24 03:32:01 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,38 @@
 import express from 'express';
+import cors from "cors";
+import mongoose from 'mongoose';
 const app = express();
+app.use(cors());
 const port = 3000;
 
+mongoose.connect('mongodb+srv://Piyush:Jethalal1.@cluster0.yc2ciyh.mongodb.net/').then((db)=>{
+  console.log("Database connected successfully!")
+})
+
+
 app.get('/',(req,res)=>{
-    res.send("It works!")
+    // res.send("<p style='color:red;'>Hello</p>")
+    const obj = {
+        names : [
+            {
+              fName: "Piyush",
+              lName: "Verma",
+            },
+            {
+              fName: "Kashish",
+              lName: "Verma",
+            },
+            {
+              fName: "Sahil",
+              lName: "Verma",
+            },
+            {
+              fName: "Sagar",
+              lName: "Verma",
+            },
+          ],
+    };
+    res.json(obj)
 })
 
 app.listen(port,()=>{
--- React/src/pages/Home/Home.jsx
+++ React/src/pages/Home/Home.jsx
@@ -1,30 +1,28 @@
 import Cards from "../../components/Cards/Cards";
+import { useEffect , useState} from "react";
+
+import axios from "axios";
+
 import "./Home.css";
-const names = [
-  {
-    fName: "Piyush",
-    lName: "Verma",
-  },
-  {
-    fName: "Kashish",
-    lName: "Verma",
-  },
-  {
-    fName: "Sahil",
-    lName: "Verma",
-  },
-  {
-    fName: "Sagar",
-    lName: "Verma",
-  },
-];
 const Home = () => {
+  const getName = ()=>{
+    axios.get("http://localhost:3000/#").then((response)=>{
+      setNames(response.data.names);
+    })
+  }
+  const [names, setNames] = useState(null);
+
+  useEffect(()=>{
+    getName();
+  },[])
   return (
     <div className="home">
       <div className="card-container">
-        {names.map((val, i) => {
-          return <Cards key={i} fName={val.fName} lName={val.lName} />;
-        })}
+        {
+          names?(names.map((val, i) => {
+            return <Cards key={i} fName={val.fName} lName={val.lName} />;
+          })):null 
+        }
       </div>
     </div>
   );
